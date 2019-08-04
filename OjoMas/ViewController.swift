f260306006b2fcb18fef44cede244ff76b66ed71
KukuhJ <kkhjmldn@gmail.com>
2019-08-05 06:28:01 +0700

    Initial Commit

---
+++ OjoMas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OjoMas
+//
+//  Created by kkhjmldn on 05/08/19.
+//  Copyright © 2019 kkhjmldn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
